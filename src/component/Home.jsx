61683d8ad7d9463514500c1013a0c3776e54e819
AngelRdz2 <131016227+AngelRdz2@users.noreply.github.com>
2026-03-22 20:01:39 -0600

    first

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const Home = ({ title = "Hola mundo" }) => {
     return (
-        <div className="p-4 border rounded-lg shadow">
-            <h2 className="text-xl font-bold">{title}</h2>
-            <p></p>
+
+        <div className="p-4 border rounded-xl shadow-md bg-white">
+            <h1 className="text-2xl font-bold text-red-500">
+
+            </h1>
+            <h2 className="text-lg text-gray-700">{title}</h2>
         </div>
     );
 };
